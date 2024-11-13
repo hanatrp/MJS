d615f9917023fc9107addc6ea65c5bae516fc586
Hana Nur Aisyah <hananurtryn@gmail.com>
2024-11-14 01:33:07 +0700

    edit map section

--- src/components/MapsSection.tsx
+++ src/components/MapsSection.tsx
@@ -17,7 +17,7 @@ const MapsSection = () => {
   useEffect(() => {
     const initMap = async () => {
       const loader = new Loader({
-        apiKey: "AIzaSyCEXW1x6XlHJCEkL9-6IAlewgYPILfP5gw",
+        apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
         version: "weekly",
       });
 
@@ -58,8 +58,8 @@ const MapsSection = () => {
         <div className="h-1/2" ref={mapRef} />
 
         {/* heading */}
-        <div className="flex flex-col gap-10">
-          <div className="flex flex-col gap-5 mx-20">
+        <div className="flex flex-col gap-10 ">
+          <div className="flex flex-col gap-5 md:mx-20 ">
             <h4 className="font-bold text-4xl text-gray-700">
               Let's Create Something new
             </h4>
@@ -70,27 +70,27 @@ const MapsSection = () => {
           </div>
 
           <div className="grid grid-cols-2 grid-rows-2 gap-10">
-            <div className="flex flex-col gap-4 items-start mx-20">
-              <FaBuilding className="text-hijau text-6xl" />
+            <div className="flex flex-col gap-4 items-start md:mx-20 ">
+              <FaBuilding className="text-hijau md:text-6xl text-4xl" />
               <h1 className="font-semibold text-xl">Headquarter</h1>
               <p className="text-gray-400 text-start">
                 The Mansion Bougenville Tower Fontana Lantai 50 Jakarta Utara
               </p>
             </div>
-            <div className="flex flex-col gap-4 items-start mx-20">
-              <FaVrCardboard className="text-hijau text-6xl" />
+            <div className="flex flex-col gap-4 items-start md:mx-20">
+              <FaVrCardboard className="text-hijau md:text-6xl text-4xl" />
               <h1 className="font-semibold text-xl">Showroom</h1>
               <p className="text-gray-400 text-start">
                 Celebration AA15/25, Grand Wisata, Bekasi, Jawa Barat
               </p>
             </div>
-            <div className="flex flex-col gap-4 items-start mx-20">
-              <FaPhoneAlt className="text-hijau text-6xl" />
+            <div className="flex flex-col gap-4 items-start md:mx-20">
+              <FaPhoneAlt className="text-hijau md:text-6xl text-4xl" />
               <h1 className="font-semibold text-xl">Phone</h1>
               <p className="text-gray-400 text-start">(+62) 811-1122-492</p>
             </div>
-            <div className="flex flex-col gap-4 items-start mx-20">
-              <FaEnvelope className="text-hijau text-6xl" />
+            <div className="flex flex-col gap-4 items-start md:mx-20">
+              <FaEnvelope className="text-hijau md:text-6xl text-4xl" />
               <h1 className="font-semibold text-xl">Email</h1>
               <p className="text-gray-400 text-start">
                 Sales@Mjsolution.co.id
